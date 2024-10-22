9fb92180fb7baec7e6f3eaeb9f99e2c1610713fb
Pedro Vicente dos Santos <44808778+PedroVicente98@users.noreply.github.com>
2024-10-22 15:07:21 -0300

    Update NetworkManager.h

--- include/NetworkManager.h
+++ include/NetworkManager.h
@@ -29,19 +29,12 @@ public:
    void startReceiving(std::shared_ptr<asio::ip::tcp::socket> socket);
    void handleMessage(std::shared_ptr<asio::ip::tcp::socket> socket, const unsigned char* data, std::size_t length);
 
-   /* void startServer(unsigned short port);
     void connectToPeer(const std::string& ip, unsigned short port);
     void sendMessage(const std::string& peer_id, const Message& message);
     void broadcastMessage(const Message& message);
 
-    void onMessageReceived(const std::function<void(const std::string&, const Message&)>& handler);
-    void onPeerConnected(const std::function<void(const std::string&)>& handler);
-    void onPeerDisconnected(const std::function<void(const std::string&)>& handler);*/
 
 private:
-    //void acceptConnections();
-    //void startReceiving(std::shared_ptr<asio::ip::tcp::socket> socket);
-
     asio::io_context io_context_;
     asio::ip::tcp::acceptor acceptor_;
     std::unordered_map<std::string, std::shared_ptr<asio::ip::tcp::socket>> peers_;
