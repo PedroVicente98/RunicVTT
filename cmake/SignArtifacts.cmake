e207050c57e1ae27c678f8c79527ebda01928673
Pedro Vicente dos Santos <44808778+PedroVicente98@users.noreply.github.com>
2025-10-08 16:07:52 -0300

    Delete cmake directory

--- cmake/SignArtifacts.cmake
+++
@@ -1,3 +0,0 @@
-# Remove development headers from the staging area before CPack zips it
-message(STATUS "[CPack] Stripping development headers from staging...")
-file(REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}/include")
